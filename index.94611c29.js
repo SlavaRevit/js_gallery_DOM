aad95b9af42b865c5243168e01c92b24294c6475
Slava Filimonenko <=>
2024-07-18 00:49:35 +0300

    make build

---
+++ index.94611c29.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),r=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){e.preventDefault();var t=e.target;r.src=t.closest("a").href});
+//# sourceMappingURL=index.94611c29.js.map
